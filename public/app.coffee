dbb366e2fe54a860b1c6cb7956b5f0c828996f4c
dmassiani <davidmassianirennes@gmail.com>
2014-10-07 11:34:27 +0200

    update various things

--- public/app.coffee
+++ public/app.coffee
@@ -1,4 +1,27 @@
 $ = require 'jquery'
 $ ->
-  $ '.container'
-  .append 'Hello World'
+
+	menu = $ '#navigation-menu'
+	menuToggle = $ '#js-mobile-menu'
+	signUp = $ '.sign-up'
+
+	$ menuToggle
+		.on 'click', ( e ) ->
+			e.preventDefault
+			menu
+				.slideToggle ->
+					if menu.is ':hidden'
+						menu.removeAttr 'style'
+
+	$ ".nav .nav-link" 
+		.click ->
+			$ ".nav .nav-link" 
+				.each ->
+						$ this
+							.removeClass "active-nav-item"
+			$ this
+				.addClass "active-nav-item"
+			$ ".nav .more"
+				.removeClass "active-nav-item"
+	false
+
--- www/app.js
+++ www/app.js
@@ -3,5 +3,24 @@ var $;
 $ = require('jquery');
 
 $(function() {
-  return $('.container').append('Hello World');
+  var menu, menuToggle, signUp;
+  menu = $('#navigation-menu');
+  menuToggle = $('#js-mobile-menu');
+  signUp = $('.sign-up');
+  $(menuToggle).on('click', function(e) {
+    e.preventDefault;
+    return menu.slideToggle(function() {
+      if (menu.is(':hidden')) {
+        return menu.removeAttr('style');
+      }
+    });
+  });
+  $(".nav .nav-link").click(function() {
+    $(".nav .nav-link").each(function() {
+      return $(this).removeClass("active-nav-item");
+    });
+    $(this).addClass("active-nav-item");
+    return $(".nav .more").removeClass("active-nav-item");
+  });
+  return false;
 });
